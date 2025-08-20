ef55795077cf0c8b5b6ea165398e2eacac1fc927
ratiorine1417 <rataventurine@gmail.com>
2025-08-20 13:18:08 +0900

    update

--- src/main/java/com/rookies4/springboottask/controller/BookRestController.java
+++ src/main/java/com/rookies4/springboottask/controller/BookRestController.java
@@ -17,9 +17,15 @@ import java.util.Optional;
 public class BookRestController {
     private final BookRepository bookRepository;
     //새 도서 등록
+//    @PostMapping
+//    public Book createBook(@RequestBody Book book) {
+//        return bookRepository.save(book);
+//    }
+    // 도서 등록
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookRepository.save(book);
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED); //CREATED 201
     }
     //모든 도서 조회
     @GetMapping
@@ -40,13 +46,27 @@ public class BookRestController {
                 .orElseThrow(() -> new BusinessException("Book Not Found", HttpStatus.NOT_FOUND));
         return existBook;
     }
+    // 저자명으로 도서 조회
+    @GetMapping("/author/{author}")
+    public List<Book> getBooksByAuthor(@PathVariable String author) {
+        return bookRepository.findByAuthor(author);
+    }
     //도서 정보 수정
-    @PutMapping("/{id}")
-    public Book updateBook(@PathVariable Long id, @RequestBody Book bookDetail) {
+//    @PutMapping("/{id}")
+//    public Book updateBook(@PathVariable Long id, @RequestBody Book bookDetail) {
+//        Book existBook = getExistBook(id);
+//        existBook.setPrice(bookDetail.getPrice());
+//        Book updateBook = bookRepository.save(existBook);
+//        return updateBook;
+//    }
+    // 도서 정보 수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetail) {
         Book existBook = getExistBook(id);
         existBook.setPrice(bookDetail.getPrice());
-        Book updateBook = bookRepository.save(existBook);
-        return updateBook;
+
+        Book updatedBook = bookRepository.save(existBook);
+        return ResponseEntity.ok(updatedBook);
     }
     //도서 삭제
     @DeleteMapping("/{id}")
@@ -55,6 +75,16 @@ public class BookRestController {
         bookRepository.delete(existBook);
         return ResponseEntity.ok("Book이 삭제되었습니다.");
     }
+    // 도서 삭제
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+//        //매칭돠는 Book 이 없으면
+//        if (!bookRepository.existsById(id)) {
+//            return ResponseEntity.notFound().build(); //404
+//        }
+//        bookRepository.deleteById(id);
+//        return ResponseEntity.noContent().build(); //204
+//    }
 
     private Book getExistBook(Long id) {
         Optional<Book> optionalBook = bookRepository.findById(id);
